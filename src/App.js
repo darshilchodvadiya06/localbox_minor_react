35a99ac95af4088f72a0cd6c48f43d82b90bfbfa
Darshil-Chodvadiya <darshilchodvadiya59@gmail.com>
2023-09-13 11:52:27 +0530

    Localbox Minor in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (task.trim() === '') return;
+    const newTask = { text: task, completed: false };
+    setTasks([...tasks, newTask]);
+    setTask('');
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTaskStatus = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <center>
+      <div className="container">
+      <h1>To-Do App</h1>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((item, index) => (
+          <li
+            key={index}
+            className={item.completed ? 'completed' : 'pending'}
+          >
+            {item.text}
+            <button onClick={() => toggleTaskStatus(index)}>
+              {item.completed ? 'Undo' : 'Complete'}
+            </button>
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => setTasks([])}>Clear All</button>
     </div>
+    </center>
   );
 }
 
